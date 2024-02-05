a59eb7d8c2386f4c038daa889c51cc2c990ebb62
Issue <101085046+MOON-CODES@users.noreply.github.com>
2024-02-05 16:46:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "29594665"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "35eb5b35ade50305777b1224fc7fe9d7"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_ID = "6391774843"
+SUPPORT_GRP = ""
+UPDATE_CHNL = ""
+OWNER_USERNAME = "DEVELOPER_K4K4SHI"
 
